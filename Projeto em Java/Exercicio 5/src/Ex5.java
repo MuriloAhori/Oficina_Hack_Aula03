d6d70cb2ecbdefa1d7093ede6357b662daf8baca
unknown <muriloahori@gmail.com>
2021-08-14 02:58:57 -0300

    Exercicios Assincronos

---
+++ Projeto em Java/Exercicio 5/src/Ex5.java
@@ -0,0 +1,17 @@
+
+public class Ex5 {
+	public static void main(String[] agrs) {
+		
+		
+		int idade1=19, idade2=18, idade3=20, idade4=21;
+		String nomeR="Raul",nomeA="Adriano", nomeBr="Bruna", nomeB="Beatriz";
+			
+		
+					
+		System.out.println(nomeB+" Tem "+idade2+" Anos");
+		System.out.println(nomeA+" Tem "+idade1+" Anos");
+		System.out.println(nomeR+" Tem "+idade3+" Anos");
+		System.out.println(nomeBr+" Tem "+idade4+" Anos");		
+					
+		}
+}
